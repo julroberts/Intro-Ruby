38810690d35a5d6e17187f74e0131060c661022d
julroberts <julroberts@hotmail.com>
2017-01-22 18:58:20 -0600

    Session-3 changes

---
+++ Methods/exercises.rb
@@ -0,0 +1,15 @@
+# exercises.rb
+
+def greeting(name)
+  return "Hello " + name
+end
+
+puts greeting("Julio")
+
+
+def scream(words)
+  words = words + '!!!!'
+  puts words
+end
+
+scream("Yippeee")
---
+++ Methods/say.rb
@@ -0,0 +1,13 @@
+# say.rb
+
+def say(words="hello")
+  puts words + '.'
+end
+
+
+say "Hi"
+say "How are you?"
+say "I'm fine'"
+
+say("That's great!'")
+say("cool!")
